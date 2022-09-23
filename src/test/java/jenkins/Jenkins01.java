3e6dd10af22954213317a9b04a6aba0b37bf6aa9
Tejashree <Tejashree@LAPTOP-OO1VLBCD>
2022-09-23 10:22:32 +0530

    1st commit

---
+++ src/test/java/jenkins/Jenkins01.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+public class Jenkins01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+
+	}
+
+}
---
+++ src/test/java/jenkins/Jenkins04.java
@@ -0,0 +1,10 @@
+package jenkins;
+
+public class Jenkins04 {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Fine.");
+	}
+
+}
